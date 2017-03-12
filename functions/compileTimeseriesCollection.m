56f4989196ff5d6955c2d286b59b6b6f8987a976
UNCCORE <cvermill@uncc.edu>
2017-03-11 19:24:58 -0500

    end of day, fixed a whole ton of bugs.

--- functions/compileTimeseriesCollection.m
+++ functions/compileTimeseriesCollection.m
@@ -4,7 +4,9 @@ outputs = loadOutputMap;
 time = tg.TimeLog;
 tsc=tscollection;
 for i = 1:length(outputs)
+    
     data = tg.OutputLog(:,i);
     tsc = addts(tsc,timeseries(data,time),outputs{i});
-end
+    
+    
 end
--- functions/runAfterSim.m
+++ functions/runAfterSim.m
@@ -1,22 +1,26 @@
 clc
 
 fprintf('\nTarget model stopped.\nExecuting data parsing and post processing.\n')
-
-fprintf('\nCompiling timeseries collection from raw data.\n')
-tsc = compileTimeseriesCollection(tg);
-
-fprintf('\nRetrieving constant parameters from model.\n')
-parameters = getParameters();
-
-fprintf('\nCalculating velocities from position data.\n')
-tsc = appendVelocities(tsc);
-
-fprintf('\nFiltering velocities.\n')
-tsc = appendFilteredSignals(tsc);
-
-fprintf('\nCalculating power factor.\n')
-tsc = appendPowerFactor(tsc);
-
-fileName =strrep(strrep(strrep(datestr(now),':',''),' ','_'),'-','');
-fprintf('\nSaving data as .\\data\\%s.\n',fileName)
-saveData
+if ~isempty(tg.OutputLog)
+    fprintf('\nCompiling timeseries collection from raw data.\n')
+    tsc = compileTimeseriesCollection(tg);
+    
+    fprintf('\nRetrieving constant parameters from model.\n')
+    parameters = getParameters();
+    
+    fprintf('\nCalculating velocities from position data.\n')
+    tsc = appendVelocities(tsc);
+    
+    fprintf('\nFiltering velocities.\n')
+    tsc = appendFilteredSignals(tsc);
+    
+    fprintf('\nCalculating power factor.\n')
+    tsc = appendPowerFactor(tsc);
+    
+    fileName =strrep(strrep(strrep(datestr(now),':',''),' ','_'),'-','');
+    fprintf('\nSaving data as .\\data\\%s.\n',fileName)
+    saveData
+    fprintf('\nDone.\n')
+else
+    fprintf('\nNo data in output log.\n')
+end
